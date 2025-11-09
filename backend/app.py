da56e89a0f5e35be2e02376595048392f09d02ca
unknown <sofialp556@gmail.com>
2025-11-09 18:00:36 -0300

    front+flask

---
+++ backend/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+from servicios.cliente_service import ClienteService
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # permite que React (localhost:3000) acceda a la API
+
+servicio = ClienteService()
+
+@app.route("/clientes", methods=["GET"])
+def listar_clientes():
+    return jsonify(servicio.listar_clientes())
+
+@app.route("/clientes", methods=["POST"])
+def crear_cliente():
+    datos = request.get_json()
+    return jsonify(servicio.crear_cliente(datos))
+
+@app.route("/clientes/buscar", methods=["GET"])
+def buscar_cliente():
+    valor = request.args.get("q", "")
+    return jsonify(servicio.buscar_clientes(valor))
+
+if __name__ == "__main__":
+    app.run(debug=True)
